841666533cc12a613f25f18e82248248bdc280bf
Mohammad Ayub Ansari <119043948+MohammadAyubAnsari@users.noreply.github.com>
2023-05-29 13:36:33 +0530

    6.Delete mail

---
+++ 6.Delete mail/src/App.js
@@ -0,0 +1,32 @@
+import LoginSignup from "./components/LoginSignup/LoginSignup";
+import ComposeEmail from "./components/Pages/Home/ComposeEmail";
+import { Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
+import InboxPage from "./components/Pages/Inbox/InboxPage";
+import Header from "./components/Pages/Home/Header";
+import ViewMessage from "./components/Pages/Inbox/ViewMessagePage";
+
+function App() {
+  const isLogin = useSelector((state) => state.auth.userAuth.isLogin);
+  console.log(isLogin);
+  return (
+    <>
+      {isLogin && <Header />}
+      <Routes>
+        {!isLogin && (
+          <Route path={"/"} element={<LoginSignup></LoginSignup>}></Route>
+        )}
+        {isLogin && (
+          <Route path={"/compose-email"} element={<ComposeEmail />}></Route>
+        )}
+        {!isLogin && <Route path={"*"} element={<LoginSignup />}></Route>}
+        {isLogin && <Route path={"*"} element={<ComposeEmail />}></Route>}
+
+        {isLogin && <Route path="inbox" element={<InboxPage />}></Route>}
+        <Route path="/inbox/:id" element={<ViewMessage />}></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
